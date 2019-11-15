82c2638d0b3fcc0cc2dcbc604ed950a7b534daef
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 11:54:22 +0800

    Update normalize.js

--- src/config/normalize.js
+++ src/config/normalize.js
@@ -2,14 +2,13 @@ import { Dimensions, Platform, PixelRatio } from 'react-native';
 
 const {
   width: SCREEN_WIDTH,
-  height: SCREEN_HEIGHT,
 } = Dimensions.get('window');
 
 // based on iphone 5s's scale
 const scale = SCREEN_WIDTH / 300;
 
 export function p(size) {
-  const newSize = size * scale 
+  const newSize = size * scale
   if (Platform.OS === 'ios') {
     return Math.round(PixelRatio.roundToNearestPixel(newSize))
   } else {
