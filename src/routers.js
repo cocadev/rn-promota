04e514d2773d5bb1e20180ba6a84f2009e13feb9
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 11:53:18 +0800

    Update routers.js

--- src/routers.js
+++ src/routers.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react'
-import { KeyboardAvoidingView, Platform, StatusBar, View, StyleSheet, Image } from 'react-native'
+import React, { PureComponent } from 'react';
+import { KeyboardAvoidingView, Platform, StatusBar, View, StyleSheet, Image } from 'react-native';
 import { Scene, Router, Stack } from 'react-native-router-flux';
 import * as Font from 'expo-font';
 import * as Permissions from 'expo-permissions';
@@ -8,7 +8,7 @@ import SelectTemplate from './screens/SelectTemplate';
 import CreateDesign from './screens/CreateDesign';
 import Home from './screens/home';
 import { screenWidth, screenHeight } from './config/static';
-import { ifIphoneX } from 'react-native-iphone-x-helper'
+import { ifIphoneX } from 'react-native-iphone-x-helper';
 
 const MyStatusBar = ({ backgroundColor, ...props }) => (
     <View style={[styles.statusBar, { backgroundColor }]}>
@@ -43,18 +43,18 @@ export default class Routers extends PureComponent {
 
     getPermissionAsync = async () => {
         if (Constants.platform.ios) {
-          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-          if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-          }
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== 'granted') {
+                alert('Sorry, we need camera roll permissions to make this work!');
+            }
         }
-      }
+    }
 
     render() {
 
         const { fontLoaded } = this.state;
-        if(!fontLoaded){
-            return <Image source={require('../assets/splash.png')} style={{ width: screenWidth, height: screenHeight}}/>
+        if (!fontLoaded) {
+            return <Image source={require('../assets/splash.png')} style={{ width: screenWidth, height: screenHeight }} />
         }
 
         return (
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     statusBar: {
-        padding:10,
+        padding: 10,
         height: 24,
         ...ifIphoneX({
             paddingTop: 50,
