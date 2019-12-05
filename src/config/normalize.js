a507482954ba0a5e296a7cbdaee958f45c93962e
PandaDev Steve <40970351+funnyjerry@users.noreply.github.com>
2019-12-05 10:05:51 +0800

    Template optimizer

--- src/config/normalize.js
+++ src/config/normalize.js
@@ -1,17 +1,14 @@
 import { Dimensions, Platform, PixelRatio } from 'react-native';
 
-const {
-  width: SCREEN_WIDTH,
-} = Dimensions.get('window');
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
-// based on iphone 5s's scale
 const scale = SCREEN_WIDTH / 300;
 
 export function p(size) {
-  const newSize = size * scale
-  if (Platform.OS === 'ios') {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize))
-  } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-  }
+	const newSize = size * scale;
+	if (Platform.OS === 'ios') {
+		return Math.round(PixelRatio.roundToNearestPixel(newSize));
+	} else {
+		return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+	}
 }
--- src/config/utils.js
+++ src/config/utils.js
@@ -7,16 +7,28 @@ import json2 from '../webviews/tem2/data.json';
 import json3 from '../webviews/tem3/data.json';
 
 class UtilService {
-    static FilterTemplate(templateId) {
-        if (templateId == '1-1-01') { return html1 }
-        if (templateId == '1-1-02') { return html2 }
-        if (templateId == '1-1-03') { return html3 }
-    };
-    static FilterJson(templateId) {
-        if (templateId == '1-1-01') { return json1 }
-        if (templateId == '1-1-02') { return json2 }
-        if (templateId == '1-1-03') { return json3 }
-    };
+	static FilterTemplate(templateId) {
+		if (templateId == '1-1-01') {
+			return html1;
+		}
+		if (templateId == '1-1-02') {
+			return html2;
+		}
+		if (templateId == '1-1-03') {
+			return html3;
+		}
+	}
+	static FilterJson(templateId) {
+		if (templateId == '1-1-01') {
+			return json1;
+		}
+		if (templateId == '1-1-02') {
+			return json2;
+		}
+		if (templateId == '1-1-03') {
+			return json3;
+		}
+	}
 }
 
-export default UtilService
+export default UtilService;
